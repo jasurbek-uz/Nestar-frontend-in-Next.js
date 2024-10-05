313a614a09219c9c406d2c3e7d6a3df9c5a6342c
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-10-05 15:57:16 +0900

    faet: develop  MyFavotires and RecentlyVisited  components logic

--- libs/components/mypage/MyFavorites.tsx
+++ libs/components/mypage/MyFavorites.tsx
@@ -5,6 +5,12 @@ import { Pagination, Stack, Typography } from '@mui/material';
 import PropertyCard from '../property/PropertyCard';
 import { Property } from '../../types/property/property';
 import { T } from '../../types/common';
+import { useMutation, useQuery } from "@apollo/client";
+import { LIKE_TARGET_PROPERTY } from "../../../apollo/user/mutation";
+import { GET_FAVORITES } from "../../../apollo/user/query";
+import { Message } from "../../enums/common.enum";
+import { Messages } from "../../config";
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from "../../sweetAlert";
 
 const MyFavorites: NextPage = () => {
 	const device = useDeviceDetect();
@@ -12,11 +18,44 @@ const MyFavorites: NextPage = () => {
 	const [total, setTotal] = useState<number>(0);
 	const [searchFavorites, setSearchFavorites] = useState<T>({ page: 1, limit: 6 });
 
-	/** APOLLO REQUESTS **/
+  /** APOLLO REQUESTS **/
+  const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+	
+
+	const {
+		loading: getFavoritesLoading,
+		data: getFavoritesData,
+		error: getFavoritesError,
+		refetch: getFavoritesRefetch,
+	} = useQuery(GET_FAVORITES, {
+		fetchPolicy: 'network-only',
+		variables: { input: searchFavorites },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+      setMyFavorites(data.getFavorites?.list);
+      setTotal(data.getFavorites?.metaCounter?.[0]?.total || 0);
+		},
+  });
+  
+
 
 	/** HANDLERS **/
 	const paginationHandler = (e: T, value: number) => {
 		setSearchFavorites({ ...searchFavorites, page: value });
+  };
+  
+  const likePropertyHandler = async (user: T, id: string) => {
+		try {
+			if (!id) return;
+			if (!user._id) throw new Error(Messages.error2);
+			// execute likeTargetProperty Mutation
+			await likeTargetProperty({ variables: { input: id } });
+			await getFavoritesRefetch({ input: searchFavorites});
+			await sweetTopSmallSuccessAlert('succes', 800);
+		} catch (err: any) {
+			console.log('Error, likePropertyHandler:', err.message);
+			sweetMixinErrorAlert(err.message).then();
+		}
 	};
 
 	if (device === 'mobile') {
@@ -33,7 +72,7 @@ const MyFavorites: NextPage = () => {
 				<Stack className="favorites-list-box">
 					{myFavorites?.length ? (
 						myFavorites?.map((property: Property) => {
-							return <PropertyCard property={property} myFavorites={true} />;
+							return <PropertyCard property={property} likePropertyHandler={likePropertyHandler} myFavorites={true} />;
 						})
 					) : (
 						<div className={'no-data'}>
--- libs/components/mypage/RecentlyVisited.tsx
+++ libs/components/mypage/RecentlyVisited.tsx
@@ -5,6 +5,8 @@ import { Pagination, Stack, Typography } from '@mui/material';
 import PropertyCard from '../property/PropertyCard';
 import { Property } from '../../types/property/property';
 import { T } from '../../types/common';
+import { useQuery } from "@apollo/client";
+import { GET_VISITED } from "../../../apollo/user/query";
 
 const RecentlyVisited: NextPage = () => {
 	const device = useDeviceDetect();
@@ -12,7 +14,22 @@ const RecentlyVisited: NextPage = () => {
 	const [total, setTotal] = useState<number>(0);
 	const [searchVisited, setSearchVisited] = useState<T>({ page: 1, limit: 6 });
 
-	/** APOLLO REQUESTS **/
+  /** APOLLO REQUESTS **/
+  
+  const {
+		loading: getViseitedLoading,
+		data: getVisitedData,
+		error: getVisitedError,
+		refetch: getVisitedRefetch,
+	} = useQuery(GET_VISITED, {
+		fetchPolicy: 'network-only',
+		variables: { input: searchVisited },
+		onCompleted: (data: T) => {
+			setRecentlyVisited(data.getVisited?.list);
+			setTotal(data.getVisited?.metaCounter?.[0]?.total || 0);
+		},
+	});
+  
 
 	/** HANDLERS **/
 	const paginationHandler = (e: T, value: number) => {
